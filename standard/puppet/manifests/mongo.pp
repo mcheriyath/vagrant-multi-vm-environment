286b63828ad65124b2624536dd8b643f2f3cca9a
Mithun Cheriyath <mithun@cheriyath.in>
2016-12-08 17:03:29 -0700

    Provisioning mongodb with puppet on vagrant

---
+++ standard/puppet/manifests/mongo.pp
@@ -0,0 +1,21 @@
+node 'mongo' {
+    group { 'puppet': ensure => present }
+
+    Exec {
+        path => [ '/bin/', '/sbin/', '/usr/bin/', '/usr/sbin/', '/usr/local/bin/', '/usr/local/node/node-default/bin' ],
+        logoutput => 'on_failure'
+    }
+
+    File { owner => 0, group => 0, mode => 0644 }
+
+    # Mongo install
+    # This should install mongodb server and client, in the system package latest version
+    class {'::mongodb::globals':
+        manage_package_repo => true,
+        server_package_name => 'mongodb-org'
+    }->
+    class {'::mongodb::server': 
+        journal => true,
+    } ->
+    class {'::mongodb::client': }
+}
